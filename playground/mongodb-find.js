6f2cd018bc53a38e248592c372aee201385ad5f3
johnvattic <mislimumer@gmail.com>
2018-10-22 12:48:43 +0300

    everything

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,12 +8,23 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true 
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
+    // db.collection('Todos').deleteMany({text: 'Drink Tea'}).then( (result) => {
+    //   console.log(result);
+    // });
+    //
+    // db.collection('Todos').deleteOne({text: 'Drink Tea'}).then( (result) => {
+    //   console.log(result);
+    // });
 
-    db.collection('Users').find({name: 'Rolyan Ali'}).toArray().then( (data) => {
-      console.log('Data: ',data);
-    }, (err) => {
-      console.log('Error: ', err);
-    })
+    db.collection('Todos').findOneAndDelete({completed: false}).then( (result) => {
+      console.log(result);
+    });
+
+    // db.collection('Users').find({name: 'Rolyan Ali'}).toArray().then( (data) => {
+    //   console.log('Data: ',data);
+    // }, (err) => {
+    //   console.log('Error: ', err);
+    // })
 
     // client.close();
 
